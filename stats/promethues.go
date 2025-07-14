65a4b503d71e9d954245e14b8443439c9b0e2fae
Joshua Snyder <josh@cheetahfox.com>
2025-07-14 04:55:51 +0000

    hmm, I guess I don't need to register those again... We opps.

--- stats/promethues.go
+++ stats/promethues.go
@@ -29,202 +29,135 @@ var (
 			Name: "total_sent",
 			Help: "Total number of packets sent",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	TotalReceived = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "total_received",
 			Help: "Total number of packets received",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	TotalLoss = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "total_loss",
 			Help: "Total number of packets lost",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	TotalDuplicates = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "total_duplicates",
 			Help: "Total number of duplicate packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Avg1000LatencyNs = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "avg_1000_latency_ns",
 			Help: "Average latency in nanoseconds for the last 1000 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Jitter1000Ns = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "jitter_1000_ns",
 			Help: "Jitter in nanoseconds for the last 1000 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Max1000LatencyNs = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "max_1000_latency_ns",
 			Help: "Maximum latency in nanoseconds for the last 1000 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Min1000LatencyNs = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "min_1000_latency_ns",
 			Help: "Minimum latency in nanoseconds for the last 1000 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Packetloss1000 = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "packetloss_1000",
 			Help: "Packet loss for the last 1000 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Avg100LatencyNs = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "avg_100_latency_ns",
 			Help: "Average latency in nanoseconds for the last 100 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Jitter100Ns = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "jitter_100_ns",
 			Help: "Jitter in nanoseconds for the last 100 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Max100LatencyNs = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "max_100_latency_ns",
 			Help: "Maximum latency in nanoseconds for the last 100 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Min100LatencyNs = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "min_100_latency_ns",
 			Help: "Minimum latency in nanoseconds for the last 100 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Packetloss100 = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "packetloss_100",
 			Help: "Packet loss for the last 100 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Avg15LatencyNs = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "avg_15_latency_ns",
 			Help: "Average latency in nanoseconds for the last 15 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Jitter15Ns = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "jitter_15_ns",
 			Help: "Jitter in nanoseconds for the last 15 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Max15LatencyNs = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "max_15_latency_ns",
 			Help: "Maximum latency in nanoseconds for the last 15 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Min15LatencyNs = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "min_15_latency_ns",
 			Help: "Minimum latency in nanoseconds for the last 15 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 	Packetloss15 = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "packetloss_15",
 			Help: "Packet loss for the last 15 packets",
 		},
-		[]string{"hostname", "ip address"},
+		[]string{"hostname", "ip_address"},
 	)
 )
-
-func init() {
-	// Register the metrics with Prometheus
-	/*
-		if err := prometheus.Register(TotalSent); err != nil {
-			fmt.Println("Error registering TotalSent metric:", err)
-		}
-	*/
-
-	prometheus.MustRegister(TotalSent)
-	prometheus.MustRegister(TotalPingsSent)
-
-	if err := prometheus.Register(TotalReceived); err != nil {
-		fmt.Println("Error registering TotalReceived metric:", err)
-	}
-	if err := prometheus.Register(TotalLoss); err != nil {
-		fmt.Println("Error registering TotalLoss metric:", err)
-	}
-	if err := prometheus.Register(TotalDuplicates); err != nil {
-		fmt.Println("Error registering TotalDuplicates metric:", err)
-	}
-	if err := prometheus.Register(Avg1000LatencyNs); err != nil {
-		fmt.Println("Error registering Avg1000LatencyNs metric:", err)
-	}
-	if err := prometheus.Register(Jitter1000Ns); err != nil {
-		fmt.Println("Error registering Jitter1000Ns metric:", err)
-	}
-	if err := prometheus.Register(Max1000LatencyNs); err != nil {
-		fmt.Println("Error registering Max1000LatencyNs metric:", err)
-	}
-	if err := prometheus.Register(Min1000LatencyNs); err != nil {
-		fmt.Println("Error registering Min1000LatencyNs metric:", err)
-	}
-	if err := prometheus.Register(Packetloss1000); err != nil {
-		fmt.Println("Error registering Packetloss1000 metric:", err)
-	}
-	if err := prometheus.Register(Avg100LatencyNs); err != nil {
-		fmt.Println("Error registering Avg100LatencyNs metric:", err)
-	}
-	if err := prometheus.Register(Jitter100Ns); err != nil {
-		fmt.Println("Error registering Jitter100Ns metric:", err)
-	}
-	if err := prometheus.Register(Max100LatencyNs); err != nil {
-		fmt.Println("Error registering Max100LatencyNs metric:", err)
-	}
-	if err := prometheus.Register(Min100LatencyNs); err != nil {
-		fmt.Println("Error registering Min100LatencyNs metric:", err)
-	}
-	if err := prometheus.Register(Packetloss100); err != nil {
-		fmt.Println("Error registering Packetloss100 metric:", err)
-	}
-	if err := prometheus.Register(Avg15LatencyNs); err != nil {
-		fmt.Println("Error registering Avg15LatencyNs metric:", err)
-	}
-	if err := prometheus.Register(Jitter15Ns); err != nil {
-		fmt.Println("Error registering Jitter15Ns metric:", err)
-	}
-	if err := prometheus.Register(Max15LatencyNs); err != nil {
-		fmt.Println("Error registering Max15LatencyNs metric:", err)
-	}
-	if err := prometheus.Register(Min15LatencyNs); err != nil {
-		fmt.Println("Error registering Min15LatencyNs metric:", err)
-	}
-	if err := prometheus.Register(Packetloss15); err != nil {
-		fmt.Println("Error registering Packetloss15 metric:", err)
-	}
-}
 
 // I hate how this is just a big list of metrics that need to be updated
 func prometheusUpdateMetrics(hostname string, pIp *ipRings) {
